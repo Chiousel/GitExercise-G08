7ff08b830674d79ad7aa71cc4c1420850f083f3f
Dhe Wei <sotongso123@gmail.com>
2025-09-21 18:53:00 +0800

    完成market功能迁移，修复所有路由和模板问题

--- project/myapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Item, ItemImage, PickupPoint
-
-admin.site.register(Item)
-admin.site.register(ItemImage)
-admin.site.register(PickupPoint)
-
-# Register your models here.
